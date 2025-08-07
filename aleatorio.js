daad03661b4736749f520c531843995895cdb4f2
igrodittrich <137417453+igrodittrich@users.noreply.github.com>
2025-08-06 22:16:46 -0300

    Delete aleatorio.js

--- aleatorio.js
+++
@@ -1,8 +0,0 @@
-const nomes = ["Igor", "Alfredo", "Bruce", "Robin", "Tony", "Judson", "Quandávio"];
-
-export function aleatorio (lista){
-    const posicao = Math.floor(Math.random()* lista.length);
-    return lista[posicao];
-}
-
-export const nome = aleatorio(nomes)
